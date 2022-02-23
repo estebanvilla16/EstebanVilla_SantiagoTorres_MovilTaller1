3f87897c5b684007039290cc06c4d5ffd4321e17
estebanVilla <estebanvilla1603@gmail.com>
2022-02-23 00:15:35 -0500

    Lista de fibonacci

--- app/src/main/java/com/example/estebanvilla_santiagotorres_moviltaller1/MainActivity.java
+++ app/src/main/java/com/example/estebanvilla_santiagotorres_moviltaller1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.estebanvilla_santiagotorres_moviltaller1;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -19,12 +20,32 @@ import com.example.estebanvilla_santiagotorres_moviltaller1.databinding.Activity
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
     Button boton;
+    private ListView lv;
+    String name,region;
+    private static String JSON_URL = "https://run.mocky.io/v3/1abbb048-745e-4913-be50-5fd8833d78a4";
+    ArrayList<HashMap<String,String>> paisesLista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,15 +58,97 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 startActivity(new Intent(MainActivity.this,Paises.class));
+
             }
 
         });
 
+        paisesLista = new ArrayList<>();
+        lv = findViewById(R.id.listaPaises);
 
+        getData getdata = new getData();
+        getdata.execute();
 
 
 
     }
+
+    public class getData extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String current = "";
+            try {
+                URL url;
+                HttpURLConnection urlConnection = null;
+                try {
+                    url = new URL(JSON_URL);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+
+                    InputStream in = urlConnection.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(in);
+
+                    int data = isr.read();
+                    while (data != -1) {
+                        current += (char) data;
+                        data = isr.read();
+                    }
+
+                    return current;
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (urlConnection != null) {
+                        urlConnection.disconnect();
+                    }
+                }
+            }catch (Exception e){
+                    e.printStackTrace();
+                }
+                return current;
+            }
+
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                JSONArray jsonArray = jsonObject.getJSONArray("Countries");
+
+                for(int i = 0; i<jsonArray.length(); i++){
+                    JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                    name = jsonObject1.getString("Name");
+                    region = jsonObject1.getString("Region");
+
+                    HashMap<String, String> countries = new HashMap<>();
+
+                    countries.put("Name", name);
+                    countries.put("Region", region);
+
+                    paisesLista.add(countries);
+
+
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this,
+                    paisesLista,
+                    R.layout.row_layaout,
+                    new String[]{"Name", "Region"},
+                    new int[] {R.id.textView3, R.id.textView4});
+
+            lv.setAdapter(adapter);
+
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
